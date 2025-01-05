518facc8813dc50adf7cad79ff8584d7bc4ba31b
michelle :) <michelleeliasflores@gmail.com>
2025-01-04 23:52:40 -0500

    Initial Commit

---
+++ DeloitteChallenge/DeloitteChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeloitteChallengeApp.swift
+//  DeloitteChallenge
+//
+//  Created by Michelle Elias Flores on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DeloitteChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
