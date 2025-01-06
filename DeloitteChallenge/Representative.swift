dd7bc7249226a6e1542d0200df6488bbc349883c
michelle :) <michelleeliasflores@gmail.com>
2025-01-05 21:11:01 -0500

    added the representatives view in order to prompt the user to find theirs (might intergrate an API to do this instead of linking to a website we'll see haha) also added the button in the tool bar of content view to lead to representatives page

---
+++ DeloitteChallenge/Representative.swift
@@ -0,0 +1,18 @@
+//
+//  Representative.swift
+//  DeloitteChallenge
+//
+//  Created by Michelle Elias Flores on 1/5/25.
+//
+
+import SwiftUI
+
+struct Representative: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Representative()
+}
